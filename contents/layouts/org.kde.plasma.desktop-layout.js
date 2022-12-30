cebb45afeb6acda56ea047cced842a6964738e55
Cappy Ishihara <cappy@cappuchino.xyz>
2022-12-30 20:55:45 +0700

    use default layout

--- contents/layouts/org.kde.plasma.desktop-layout.js
+++ contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,35 +1,7 @@
-var plasma = getApiVersion(1);
-
-var layout = {
-    "desktops": [
-        {
-            "applets": [
-            ],
-            "config": {
-                "/": {
-                    "ItemGeometries-1920x1001": "",
-                    "ItemGeometriesHorizontal": "",
-                    "formfactor": "0",
-                    "immutability": "1",
-                    "lastScreen": "0",
-                    "wallpaperplugin": "org.kde.image"
-                },
-                "/General": {
-                    "ToolBoxButtonState": "topcenter",
-                    "ToolBoxButtonX": "559",
-                    "ToolBoxButtonY": "34"
-                },
-                "/Wallpaper/org.kde.image/General": {
-                    "Image": "file:///home/cappy/3840x2160.png"
-                }
-            },
-            "wallpaperPlugin": "org.kde.image"
-        }
-    ],
-    "panels": [
-    ],
-    "serializationFormatVersion": "1"
+loadTemplate("org.kde.plasma.desktop.defaultPanel")
+ 
+var desktopsArray = desktopsForActivity(currentActivity());
+for( var j = 0; j < desktopsArray.length; j++) {
+    desktopsArray[j].wallpaperPlugin = 'org.kde.image';
 }
-;
-
-plasma.loadSerializedLayout(layout);
+ 
